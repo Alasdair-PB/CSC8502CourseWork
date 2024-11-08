7f30a8007f8bc50c58008da027c12aff79a5ef64
Alasdair PB <39963762+Alasdair-PB@users.noreply.github.com>
2024-11-08 17:50:08 +0000

    Fresnel Setup

--- Coursework/Shaders/TexturedFragment.glsl
+++ Coursework/Shaders/TexturedFragment.glsl
@@ -1,7 +1,7 @@
 #version 330 core
 
 uniform sampler2D diffuseTex;  
-uniform vec3 cameraPosition;   
+uniform vec3 cameraPos;   
 
 in Vertex{
     vec4 colour;
@@ -13,12 +13,12 @@ out vec4 fragColour;
 
 void main(void) 
 {
-    vec3 viewDir = normalize(cameraPosition - IN.normal); 
+    vec3 viewDir = normalize(cameraPos - IN.normal); 
     float cosTheta = dot(IN.normal, viewDir); 
     float fresnel = pow(1.0 - cosTheta, 5.0);  
 
     vec4 diffuse = texture(diffuseTex, IN.texCoord);  
-        vec4 rimColor = vec4(fresnel, fresnel, fresnel, 1.0); 
+    vec4 rimColor = vec4(fresnel, fresnel, fresnel, 1.0); 
 
-    fragColour = diffuse + rimColor; 
+    fragColour = mix(diffuse, rimColor, 0.5f); 
 }
